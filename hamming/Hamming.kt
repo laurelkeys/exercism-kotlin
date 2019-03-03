0a52b4454a18db246e37b2f23d3fffba07e5a25a
Tiago <tiagoloureirochaves@gmail.com>
2019-03-03 19:49:34 -0300

    Add hamming

---
+++ hamming/Hamming.kt
@@ -0,0 +1,6 @@
+object Hamming {
+    fun compute(dna1: String, dna2: String): Int {
+        require(dna1.length == dna2.length) { "left and right strands must be of equal length." }
+        return dna1.zip(dna2).count { (nucleotide1, nucleotide2) -> nucleotide1 != nucleotide2 }
+    }
+}
