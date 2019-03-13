e46b64a3ba3526c2f3a63384282e61d672b28309
Tiago <tiagoloureirochaves@gmail.com>
2019-03-13 23:19:18 +0000

    Solve saddle-points using collection's intersect

--- saddle-points/Matrix.kt
+++ saddle-points/Matrix.kt
@@ -1,21 +1,25 @@
 class Matrix(private val rows: List<List<Int>>) {
 
     private val size = rows[0].size
-
-    private val maxRowValues = rows.map { it.max() ?: 0 }
-
-    private val minColumnValues = with(0 until this.size) {
-        map { j -> map { i -> rows[i][j] }.min() ?: 0 }
-    }
+    private val columns by lazy { (0 until size).map { i -> rows.map { it[i] } } }
 
     val saddlePoints: Set<MatrixCoordinate> by lazy {
+        maxRowValues.intersect(minColumnValues)
+    }
+
+    private val maxRowValues by lazy {
         rows.mapIndexed { i, row ->
-            row.mapIndexedNotNull { j, it ->
-                if (it >= maxRowValues[i] && it <= minColumnValues[j])
-                    MatrixCoordinate(i, j)
-                else
-                    null
-            }
+            row.withIndex()
+                    .filter { (_, it) -> it == row.max() }
+                    .map { MatrixCoordinate(i, it.index) }
+        }.flatten().toSet()
+    }
+
+    private val minColumnValues by lazy {
+        columns.mapIndexed { j, column ->
+            column.withIndex()
+                    .filter { (_, it) -> it == column.min() }
+                    .map { MatrixCoordinate(it.index, j) }
         }.flatten().toSet()
     }
 }
